31af374dc555476848d5a12c094c74c03490aaba
Magudesh K <74949557+Magudeshgit@users.noreply.github.com>
2024-11-09 00:15:53 +0530

    Update query.java

--- src/internals/db/query.java
+++ src/internals/db/query.java
@@ -53,36 +53,3 @@ public class query {
     }
     
 }
-
-// import java.net.http.HttpRequest;
-// import java.net.http.HttpRequest.BodyPublishers;
-// import java.net.URI;
-// import java.net.http.HttpResponse;
-// import java.net.http.HttpClient;
-// // DBID: 61afa940-e3a1-466b-8828-71c4c0d25081;
-// // ACCID: 208d0f24df52505b5e34ad8d4adaf567
-// // -iBAoKoLIzQbuQ_X7p-AEh7J1LdhVUJK3ht0iPRa
-
-// public class projectx {
-//     public static void main(String[] args)
-//     {
-//         HttpRequest post = HttpRequest.newBuilder()
-//         .uri(URI.create("https://api.cloudflare.com/client/v4/accounts/208d0f24df52505b5e34ad8d4adaf567/d1/database/61afa940-e3a1-466b-8828-71c4c0d25081/query"))
-//         .header("Authorization", "Bearer -iBAoKoLIzQbuQ_X7p-AEh7J1LdhVUJK3ht0iPRa")
-//         .POST(BodyPublishers.ofString("{\"sql\": \"select * from comments\"}"))
-//         .build();
-//         System.out.println("Gone");
-
-//         HttpClient client = HttpClient.newHttpClient();
-//         try
-//         {
-//             HttpResponse<String> response = client.send(post, HttpResponse.BodyHandlers.ofString());
-//             System.out.println(response.body());
-//         }
-//         catch (Exception e)
-//         {
-//             e.printStackTrace();
-//         }
-//     }
-
-// }
